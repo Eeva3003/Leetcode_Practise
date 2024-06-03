1a5023970c7a0e72bd5eb400a3bedc4e4d09a3f1
Eeva3003 <97470217+Eeva3003@users.noreply.github.com>
2024-06-03 23:22:08 +0530

    Create two sum.py
    using  hashmap

---
+++ two sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash={}
+        for i in range(len(nums)):
+            comp=target-nums[i]
+            if comp in hash:
+                return [hash[comp],i]
+            hash[nums[i]]=i    
+
+        return []    
+
+        
