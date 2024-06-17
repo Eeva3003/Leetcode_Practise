b5938f01e7abbca15383973f4a8730edc0d3ada4
Eeva3003 <97470217+Eeva3003@users.noreply.github.com>
2024-06-17 14:25:22 +0530

    Update max number of k sum pairs.py

--- Leetcode75/max number of k sum pairs.py
+++ Leetcode75/max number of k sum pairs.py
@@ -20,5 +20,24 @@ class Solution:
                     hash1[k-i]-=1
         
         return count             
+#better complexity
+nums.sort()
+
+        left = 0 
+        right = len(nums) - 1
+        operation = 0 
+
+        while left < right:
+            if ((nums[left] + nums[right]) == k):
+                operation += 1
+                left +=1 
+                right -=1
+            elif((nums[left] + nums[right]) < k):
+                left += 1
+            else:
+                right -= 1
+        return operation           
+
+        
 
         
