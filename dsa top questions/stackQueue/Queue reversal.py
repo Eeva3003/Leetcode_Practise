68f3c9a799f020705e19b311c6e8c3d69d14856c
Eeva3003 <97470217+Eeva3003@users.noreply.github.com>
2024-06-26 17:02:03 +0530

    Create Queue reversal.py

---
+++ dsa top questions/stackQueue/Queue reversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    #Function to reverse the queue.
+    def rev(self, q):
+        stack=[]
+        while (not q.empty()):
+            stack.append(q.queue[0])
+            q.get()
+        while (len(stack)!=0):
+            q.put(stack[-1])
+            stack.pop()
+        return q        
